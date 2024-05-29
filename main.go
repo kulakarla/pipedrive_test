986f64d3af7f314b76641e8a5aa4eb85d1bc7960
kulakarla <sderrtsee@gmail.com>
2024-05-30 00:55:31 +0300

    added totalrequests, total and mean latency&reqduration

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 )
 
+type MethodMetrics struct {
+	TotalRequests int64   `json:"total_requests"`
+	MeanDuration  float64 `json:"mean_duration"`
+	TotalDuration int64   `json:"total_duration"`
+	MeanLatency   float64 `json:"mean_latency"`
+	TotalLatency  int64   `json:"total_latency"`
+}
+
+type Metrics struct {
+	GET  MethodMetrics `json:"GET"`
+	POST MethodMetrics `json:"POST"`
+	PUT  MethodMetrics `json:"PUT"`
+}
+
 var (
+	metrics       Metrics
+	mu            sync.Mutex
 	apiToken      = "863be942d8456f146e61026f7cf69dc78efda801"
 	apiTokenParam = "?api_token=" + apiToken
 	baseUrl       = "https://api.pipedrive.com/v1/deals/"
 )
 
 func main() {
-	http.HandleFunc("/deals", handler)
-	http.HandleFunc("/deals/", handler)
+	http.HandleFunc("/deals", requestMetricsMiddleware(handler))
+	http.HandleFunc("/deals/", requestMetricsMiddleware(handler))
+	http.HandleFunc("/metrics", metricsHandler)
 	log.Println("Server listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
@@ -44,11 +64,13 @@ func proxyRequest(rw http.ResponseWriter, req *http.Request) {
 
 	proxyReq.Header = req.Header.Clone()
 
+	startLatency := time.Now()
 	proxyResp, err := client.Do(proxyReq)
 	if err != nil {
 		http.Error(rw, "Failed to get response from target URL", http.StatusInternalServerError)
 		return
 	}
+	latency := time.Since(startLatency).Milliseconds()
 
 	defer proxyResp.Body.Close()
 
@@ -65,4 +87,59 @@ func proxyRequest(rw http.ResponseWriter, req *http.Request) {
 		http.Error(rw, "Failed to copy response body", http.StatusInternalServerError)
 	}
 
+	updateMetrics(req.Method, latency)
+
+}
+
+func updateMetrics(method string, latency int64) {
+	mu.Lock()
+	defer mu.Unlock()
+	var methodMetrics *MethodMetrics
+	switch method {
+	case http.MethodGet:
+		methodMetrics = &metrics.GET
+	case http.MethodPost:
+		methodMetrics = &metrics.POST
+	case http.MethodPut:
+		methodMetrics = &metrics.PUT
+	}
+
+	if methodMetrics != nil {
+		methodMetrics.TotalRequests++
+		methodMetrics.TotalLatency += latency
+		methodMetrics.MeanLatency = float64(methodMetrics.TotalLatency) / float64(methodMetrics.TotalRequests)
+	}
+}
+
+func requestMetricsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(rw, req)
+		duration := time.Since(start).Milliseconds()
+
+		mu.Lock()
+		defer mu.Unlock()
+
+		var methodMetrics *MethodMetrics
+		switch req.Method {
+		case http.MethodGet:
+			methodMetrics = &metrics.GET
+		case http.MethodPost:
+			methodMetrics = &metrics.POST
+		case http.MethodPut:
+			methodMetrics = &metrics.PUT
+		}
+
+		if methodMetrics != nil {
+			methodMetrics.TotalDuration += duration
+			methodMetrics.MeanDuration = float64(methodMetrics.TotalDuration) / float64(methodMetrics.TotalRequests)
+		}
+	}
+}
+
+func metricsHandler(rw http.ResponseWriter, req *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(metrics)
 }
