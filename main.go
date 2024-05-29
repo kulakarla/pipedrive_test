02df6ee4eade12d88b313441fdb3277ac02d7824
kulakarla <sderrtsee@gmail.com>
2024-05-30 01:17:00 +0300

    Initial logging added, metrics working

--- main.go
+++ main.go
@@ -36,17 +36,24 @@ func main() {
 	http.HandleFunc("/deals", requestMetricsMiddleware(handler))
 	http.HandleFunc("/deals/", requestMetricsMiddleware(handler))
 	http.HandleFunc("/metrics", metricsHandler)
-	log.Println("Server listening on localhost:8080")
+	http.HandleFunc("/", invalidPathHandler)
+	log.Println("Server listening on localhost:8080..")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
+func getCurrentTime() string {
+	return time.Now().Format(time.RFC3339)
+}
+
 func handler(rw http.ResponseWriter, req *http.Request) {
+	log.Printf("%s Handling %s request for %s\n", getCurrentTime(), req.Method, req.URL.Path)
 	switch req.Method {
 	case http.MethodGet, http.MethodPost, http.MethodPut:
 		proxyRequest(rw, req)
 	default:
 		http.Error(rw, "Method "+req.Method+" not allowed", http.StatusMethodNotAllowed)
+		log.Printf("%s Method %s not allowed for %s\n", getCurrentTime(), req.Method, req.URL.Path)
 	}
 }
 
@@ -56,9 +63,11 @@ func proxyRequest(rw http.ResponseWriter, req *http.Request) {
 	path := strings.TrimPrefix(req.URL.Path, "/deals")
 	targetURL := baseUrl + path + apiTokenParam
 
+	log.Printf("%s Proxy request handling started for %s %s", getCurrentTime(), req.Method, req.URL.Path)
 	proxyReq, err := http.NewRequest(req.Method, targetURL, req.Body)
 	if err != nil {
 		http.Error(rw, "Failed to create request", http.StatusInternalServerError)
+		log.Printf("%s Failed to create request for %s: %v\n", getCurrentTime(), targetURL, err)
 		return
 	}
 
@@ -68,8 +77,10 @@ func proxyRequest(rw http.ResponseWriter, req *http.Request) {
 	proxyResp, err := client.Do(proxyReq)
 	if err != nil {
 		http.Error(rw, "Failed to get response from target URL", http.StatusInternalServerError)
+		log.Printf("%s Failed to get response from %s: %v\n", getCurrentTime(), targetURL, err)
 		return
 	}
+
 	latency := time.Since(startLatency).Milliseconds()
 
 	defer proxyResp.Body.Close()
@@ -85,9 +96,11 @@ func proxyRequest(rw http.ResponseWriter, req *http.Request) {
 	_, err = io.Copy(rw, proxyResp.Body)
 	if err != nil {
 		http.Error(rw, "Failed to copy response body", http.StatusInternalServerError)
+		log.Printf("%s Failed to copy response body for %s: %v\n", getCurrentTime(), targetURL, err)
 	}
 
 	updateMetrics(req.Method, latency)
+	log.Printf("%s Request successfully completed: %s %s\n", getCurrentTime(), req.Method, req.URL.Path)
 
 }
 
@@ -138,8 +151,16 @@ func requestMetricsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func metricsHandler(rw http.ResponseWriter, req *http.Request) {
+	log.Printf("%s Handling %s request for %s", getCurrentTime(), req.Method, req.URL.Path)
 	mu.Lock()
 	defer mu.Unlock()
 	rw.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(metrics)
+
+	log.Printf("%s Successfully returned metrics", getCurrentTime())
+}
+
+func invalidPathHandler(rw http.ResponseWriter, req *http.Request) {
+	http.Error(rw, "Invalid path", http.StatusNotFound)
+	log.Printf("%s Invalid path accessed: %s\n", getCurrentTime(), req.URL.Path)
 }
