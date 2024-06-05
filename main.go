51a70e0943d21643888f650ad6b7d81a09a1553c
kulakarla <sderrtsee@gmail.com>
2024-06-05 18:05:36 +0300

    fixed GET /metrics and GET /metrics/, added more tests

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/deals", handlers.RequestMetricsMiddleware(handlers.Handler))
 	http.HandleFunc("/deals/", handlers.RequestMetricsMiddleware(handlers.Handler))
 	http.HandleFunc("/metrics", handlers.MetricsHandler)
+	http.HandleFunc("/metrics/", handlers.MetricsHandler)
 	http.HandleFunc("/", handlers.InvalidPathHandler)
 	log.Println("Server listening on localhost:8080..")
 	log.Fatal(http.ListenAndServe(":8080", nil))
