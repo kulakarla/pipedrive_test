48aef42809de86a705c0d48c25bbff3377013ee0
kulakarla <sderrtsee@gmail.com>
2024-06-05 17:33:12 +0300

    hmm

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 //APIToken is the API token used for authenticating the requests to the official PipeDrive API
-//Please change this to your specific API Token!
+//Please change this to your specific API Token
 const APIToken = "863be942d8456f146e61026f7cf69dc78efda801"
 
 //APITokenParam combines the URL parameter with the token
